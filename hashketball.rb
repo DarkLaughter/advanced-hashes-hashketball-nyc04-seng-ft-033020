947928cbd6430e87ec11bd9836af7785682c5a2f
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 23:11:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ def player_stats(name)
     teams[:players].each do |player|
 
         if player[:player_name] == name
-            player.each {|key| stats[key] = value}
+            player.each {|key, value| stats[key] = value}
         end
       end
     end
